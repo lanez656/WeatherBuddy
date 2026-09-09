86ab947ede01897520af0f9215fc5ffa8e24721b
Peter Tingskov <s255415@student.dtu.dk>
2026-09-09 08:31:54 +0200

    Test

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -5,5 +5,5 @@ namespace WeatherBuddy.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    public partial string Greeting { get; set; } = "Welcome to WeatherBuddy!";
 }
